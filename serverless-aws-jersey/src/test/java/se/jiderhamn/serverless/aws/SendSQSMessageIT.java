d1e5778d7e6fa79158d4e331b46bb0df566d7fb4
Mattias Jiderhamn <mattias.jiderhamn@consultant.volvo.com>
2018-08-15 07:25:44 +0200

    Use file as test case input

--- serverless-aws-jersey/src/test/java/se/jiderhamn/serverless/aws/SendSQSMessageIT.java
+++ serverless-aws-jersey/src/test/java/se/jiderhamn/serverless/aws/SendSQSMessageIT.java
@@ -4,7 +4,10 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * @author Mattias Jiderhamn
@@ -14,30 +17,10 @@ public class SendSQSMessageIT {
   private static final String INPUT_QUEUE = "https://sqs.eu-central-1.amazonaws.com/047533455857/50shades-input";
 
   @Test
-  public void sendSQSMessage() {
+  public void sendSQSMessage() throws IOException {
+    final String input = IOUtils.toString(getClass().getResourceAsStream("input.xml"));
     AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-    sqs.sendMessage(INPUT_QUEUE, "<?xml version=\"1.0\"?>\n" +  // TODO Read file instead
-        "<order>\n" +
-        "  <head>\n" +
-        "    <customer>\n" +
-        "      <id>123</id>\n" +
-        "    </customer>\n" +
-        "  </head>\n" +
-        "  <lines>\n" +
-        "    <line>\n" +
-        "      <article>\n" +
-        "        <id>FOO</id>\n" +
-        "      </article>\n" +
-        "      <quantity>3</quantity>\n" +
-        "    </line>\n" +
-        "    <line>\n" +
-        "      <article>\n" +
-        "        <id>BAR</id>\n" +
-        "      </article>\n" +
-        "      <quantity>1</quantity>\n" +
-        "    </line>\n" +
-        "  </lines>\n" +
-        "</order>");
+    sqs.sendMessage(INPUT_QUEUE, input);
   }
   
   @Test
