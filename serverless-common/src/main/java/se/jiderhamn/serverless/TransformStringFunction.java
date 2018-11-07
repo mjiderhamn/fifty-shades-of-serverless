946554fb5565036c8a0f19a77c369380a3fdfddb
Mattias Jiderhamn <mattias@jiderhamn.se>
2018-11-07 21:51:41 +0100

    Spring Cloud Function: Move POF to serverless-common

--- serverless-common/src/main/java/se/jiderhamn/serverless/TransformStringFunction.java
+++ serverless-common/src/main/java/se/jiderhamn/serverless/TransformStringFunction.java
@@ -1,11 +1,9 @@
-package se.jiderhamn.serverless.spring;
-
-import se.jiderhamn.serverless.TransformationService;
+package se.jiderhamn.serverless;
 
 import java.util.function.Function;
 
 @SuppressWarnings("unused")
-public class TransformFunction implements Function<String, String> {
+public class TransformStringFunction implements Function<String, String> {
   @Override
   public String apply(String input) {
     return new String(TransformationService.transform(input.getBytes()));
